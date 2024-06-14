32a05508bb070878b6692a198169fa8f28d3e9ca
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-06-14 23:39:44 +0600

    create routes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
             <h4>This is header</h4>
+            <Link to='/'>home</Link>
+            <Link to='/login'>login</Link>
         </div>
     );
 };
